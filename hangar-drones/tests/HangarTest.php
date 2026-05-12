030b82ffe8b6a1f9b00f9221c5ba3e9d0fdab6aa
Your Name <you@example.com>
2026-05-12 16:19:06 +0200

    Hangar test: can't add

--- hangar-drones/tests/HangarTest.php
+++ hangar-drones/tests/HangarTest.php
@@ -53,4 +53,49 @@ final class HangarTest extends TestCase
         $this->assertTrue($hangar->hasFreeSlot());
     }
 
+    public function testCannotAddRetiredDrone(): void
+    {
+        $hangar = new Hangar(2);
+        $drone = new Drone('DR-103', 0, Drone::STATUS_RETIRED);
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Cannot add drone DR-103 with status retired');
+
+        $hangar->addDrone($drone);
+    }
+
+    public function testCannotAddInFlightDrone(): void
+    {
+        $hangar = new Hangar(2);
+        $drone = new Drone('DR-104', 0, Drone::STATUS_IN_FLIGHT);
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Cannot add drone DR-104 with status in_flight');
+
+        $hangar->addDrone($drone);
+    }
+
+    public function testCannotAddDuplicateDroneById(): void
+    {
+        $hangar = new Hangar(2);
+        $hangar->addDrone(new Drone('DR-105'));
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('Drone DR-105 is already known by this hangar');
+
+        $hangar->addDrone(new Drone('DR-105'));
+    }
+
+    public function testCannotAddWhenNoFreeSlots(): void
+    {
+        $hangar = new Hangar(1);
+        $hangar->addDrone(new Drone('DR-106'));
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('No free slots available');
+
+        $hangar->addDrone(new Drone('DR-107', 0, Drone::STATUS_MAINTENANCE));
+    }
+
+
 }
